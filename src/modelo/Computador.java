aba2720aaecc2298983157973bf79117604fa83b
AlejandroC23 <alejandro.cevallos919@ist17dejulio.edu.ec>
2024-06-17 14:45:14 -0500

    Inicialización de clases: Tienda y Computadora

--- src/modelo/Computador.java
+++ src/modelo/Computador.java
@@ -6,8 +6,75 @@ package modelo;
 
 /**
  *
- * @author PC07
+ * @author Alejandro
  */
 public class Computador {
+    public String adclMarca;
+    public int adclMemoria;
+    public String adclProcesador;
+    public String adclSistemaOperativo;
+    public double adclPrecio;
     
+    //Métodos Constructores
+    public Computador(){}
+
+    public Computador(String adclMarca, int adclMemoria, String adclProcesador, String adclSistemaOperativo, double adclPrecio) {
+        this.adclMarca = adclMarca;
+        this.adclMemoria = adclMemoria;
+        this.adclProcesador = adclProcesador;
+        this.adclSistemaOperativo = adclSistemaOperativo;
+        this.adclPrecio = adclPrecio;
+    }
+    
+    //Métodos de Encapsulamiento
+
+    public String getAdclMarca() {
+        return adclMarca;
+    }
+
+    public void setAdclMarca(String adclMarca) {
+        this.adclMarca = adclMarca;
+    }
+
+    public int getAdclMemoria() {
+        return adclMemoria;
+    }
+
+    public void setAdclMemoria(int adclMemoria) {
+        this.adclMemoria = adclMemoria;
+    }
+
+    public String getAdclProcesador() {
+        return adclProcesador;
+    }
+
+    public void setAdclProcesador(String adclProcesador) {
+        this.adclProcesador = adclProcesador;
+    }
+
+    public String getAdclSistemaOperativo() {
+        return adclSistemaOperativo;
+    }
+
+    public void setAdclSistemaOperativo(String adclSistemaOperativo) {
+        this.adclSistemaOperativo = adclSistemaOperativo;
+    }
+
+    public double getAdclPrecio() {
+        return adclPrecio;
+    }
+
+    public void setAdclPrecio(double adclPrecio) {
+        this.adclPrecio = adclPrecio;
+    }
+    
+    //Métodos de Regla de Negocio
+    public void imprimir(){
+        System.out.println("******** INFO. COMPUTADORA ********\n" +
+                "Marca: " + getAdclMarca() + "\n" +
+                "Cantidad de Memoria: " + getAdclMemoria()+ "GB\n" +
+                "Característica del Procesador: " + getAdclProcesador()+ "\n" +
+                "Sistema Operativo: " + getAdclSistemaOperativo()+ "\n" +
+                "Precio: " + getAdclPrecio() + "\n");
+    }
 }
--- src/modelo/Tienda.java
+++ src/modelo/Tienda.java
@@ -6,8 +6,52 @@ package modelo;
 
 /**
  *
- * @author PC07
+ * @author Alejandro
  */
 public class Tienda {
+    public String adclNombreTienda;
+    public String adclPropietario;
+    public int adclIdTrib;
     
+    //Métodos Constructores
+    public Tienda(){}
+    
+    public Tienda(String adclNombreTienda, String adclPropietario, int adclIdTrib) {
+        this.adclNombreTienda = adclNombreTienda;
+        this.adclPropietario = adclPropietario;
+        this.adclIdTrib = adclIdTrib;
+    }
+    
+    //Métodos de Encapsluamiento
+    public String getAdclNombreTienda() {
+        return adclNombreTienda;
+    }
+
+    public void setAdclNombreTienda(String adclNombreTienda) {
+        this.adclNombreTienda = adclNombreTienda;
+    }
+
+    public String getAdclPropietario() {
+        return adclPropietario;
+    }
+
+    public void setAdclPropietario(String adclPropietario) {
+        this.adclPropietario = adclPropietario;
+    }
+
+    public int getAdclIdTrib() {
+        return adclIdTrib;
+    }
+
+    public void setAdclIdTrib(int adclIdentificadorTrib) {
+        this.adclIdTrib = adclIdentificadorTrib;
+    }
+    
+    //Métodos de Reglas de Negocio
+    public void imprimir(){
+        System.out.println("********* INFO. TIENDA *********\n" +
+                "Nombre: " + getAdclNombreTienda() + "\n" +
+                "Propietario: " + getAdclPropietario()+ "\n" +
+                "Identificador Tributario: " + getAdclIdTrib()+ "\n");
+    }
 }
